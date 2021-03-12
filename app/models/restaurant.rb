7c53c6177c15bdfcab892acea2fb1df88fa87b6c
bghalami <benjaminghalami@gmail.com>
2021-03-11 22:52:25 -0700

    Add migration to add category to restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ApplicationRecord
-  validates_presence_of :name, :street_address, :post_code, :number_of_chairs
-  
+  validates_presence_of :name,
+                        :street_address,
+                        :post_code,
+                        :number_of_chairs,
+                        :category
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_054234) do
+ActiveRecord::Schema.define(version: 2021_03_12_054832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "cafes", force: :cascade do |t|
-    t.string "cafe_name"
-    t.string "street_address"
-    t.string "post_code"
-    t.integer "number_of_chairs"
-  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "street_address"
     t.string "post_code"
     t.integer "number_of_chairs"
+    t.string "category"
   end
 
 end
